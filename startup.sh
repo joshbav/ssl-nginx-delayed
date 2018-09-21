1885efb422556253d5ea4796356c358abb5d3112
“joshbav” <josh@Joshs-Mac.frontierlocal.net>
2018-09-21 11:14:22 -0700

    scripted commit 09-21-18

--- startup.sh
+++ startup.sh
@@ -4,23 +4,23 @@
 # The goal is to simulate a slow starting app that is enabled for graceful shutdown.
 # By JoshB @ Mesosphere, Revision 9-20-18
 
-# A signal handler to shutdown cleanly.
-# App definition has a shutdown delay (taskKillGracePeriodSeconds) of 60 seconds, 
-# thus after sigterm is sent marathon will give the app 60 seconds to shutdown before sigkill is sent.
-# During that time NGINX will no long provide it's /healthy endpoint
 shutdown() {
+  # A signal handler to shutdown cleanly.
+  # App definition has a shutdown delay (taskKillGracePeriodSeconds) of 60 seconds,
+  # thus after sigterm is sent marathon will give the app 60 seconds to shutdown before sigkill is sent.
+  # During that time NGINX will no long provide it's /healthy endpoint
   echo
   echo $(date)
   echo "Either the SIGTERM signal was recieved from marathon, or the this startup script" 
   echo "called this shutdown function because $RUN_TIME_SECONDS seconds has elapsed."
   echo
-  echo "Shutting down gracefully."
+  echo "Shutting down gracefully in $SIGKILL_DELAY_SECONDS seconds."
   echo "Removing /healthy.html endpoint /usr/share/nginx/html/healthy.html"
   rm -f /usr/share/nginx/html/healthy.html
   echo
-  echo "Sleeping for 45 seconds so health check can fail and the load balancer can"
+  echo "Sleeping for $SIGKILL_DELAY_SECONDS seconds so health check can fail and the load balancer can"
   echo "withdrawl the app."
-  sleep 45
+  sleep SIGKILL_DELAY_SECONDS
   echo
   echo Exiting
   echo
@@ -43,7 +43,7 @@ EXTRA_STARTUP_DELAY=$(cat /dev/urandom | tr -dc '0-9' | fold -w 1 | head -n 1)
 echo
 if [ -z "$STARTUP_DELAY_SECONDS" ] 
 then
-   echo No STARTUP_DELAY_SECONDS varible set, defaulting to 30 seconds
+   echo No STARTUP_DELAY_SECONDS varible set, defaulting to 30.
    STARTUP_DELAY_SECONDS=30
 fi
 
@@ -56,22 +56,32 @@ echo NGINX will be started in $STARTUP_DELAY_SECONDS seconds
 echo
 if [ -z "$RUN_TIME_SECONDS" ] 
 then
-   echo No RUN_TIME_SECONDS varible set, defaulting to 180
+   echo No RUN_TIME_SECONDS varible set, defaulting to 180.
    RUN_TIME_SECONDS=180
 fi
 
 EXTRA_RUN_TIME=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
 echo RUN_TIME_SECONDS is $RUN_TIME_SECONDS
 echo EXTRA_RUN_TIME is $EXTRA_RUN_TIME
+echo EXTRA_RUN_TIME is added to avoid all instances termianting at the same time.
 RUN_TIME_SECONDS=$((RUN_TIME_SECONDS + $EXTRA_RUN_TIME))
-echo "NGINX will be ran for $RUN_TIME_SECONDS seconds"
+echo "NGINX will be ran for $RUN_TIME_SECONDS seconds."
+
+echo
+if [ -z "$SIGKILL_DELAY_SECONDS" ]
+then
+   echo No SIGKILL_DELAY_SECONDS varible set, defaulting to 45.
+   SIGKILL_DELAY_SECONDS=45
+fi
+
+echo SIGKILL_DELAY_SECONDS is $SIGKILL_DELAY_SECONDS seconds.
 
 echo
 echo "Now sleeping for $STARTUP_DELAY_SECONDS seconds before starting NGINX."
 echo "This simulates a slow starting app."
 sleep $STARTUP_DELAY_SECONDS
 echo
-echo Done sleeping, now starting NGINX
+echo Done sleeping, now starting NGINX. Ignore the SSL warning. 
 echo
 # Starting NGINX in its default daemon mode, thus execution will return to this script
 nginx
@@ -79,5 +89,7 @@ echo
 echo "NGINX started, shutting down in $RUN_TIME_SECONDS seconds." 
 echo
 sleep $RUN_TIME_SECONDS
+echo
+echo RUN_TIME_SECONDS has elapsed, now calling SIGTERM handler function.
 shutdown
 
